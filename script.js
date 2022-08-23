416796435f3f54d537c6550db113bfe8fc333742
Roy Romero <Romerorr94@gmail.com>
2022-08-22 22:29:49 -0400

    Added display and game controllers

---
+++ script.js
@@ -0,0 +1,34 @@
+const player = (name, symbol) =>{
+
+
+    return{name, symbol}
+
+}
+
+const gameBoard = (gameBoard) => {
+
+    gameBoard = ["", "", "", "", "", "", "", "", ""]
+
+    return {gameBoard}
+
+}
+
+const displayController = (() =>{
+
+
+    const restartButton = document.getElementById("restart-button");
+    const fieldElements = document.querySelectorAll(".piece");
+
+
+
+
+
+})
+
+
+const gameController = (() =>{
+
+
+
+
+})
